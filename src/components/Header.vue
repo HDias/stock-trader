1507cc405ef5f324859dd26cdb1e9f466ab3efee
Horecio Araujo Dias <horecio@gmail.com>
2022-09-05 21:37:25 -0300

    Create toolbarr

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,34 @@
 <template>
-  <h1>Header</h1>
+  <v-toolbar app>
+    <v-toolbar-title class="headline text-uppercase mr-4">
+      <span>Stock</span>
+      <span class="font-weight-light">Trader</span>
+    </v-toolbar-title>
+
+    <v-toolbar-items>
+      <v-btn flat to="/">Início</v-btn>
+      <v-btn flat to="/portfolio">Portifólio</v-btn>
+      <v-btn flat to="/stocks">Ações</v-btn>
+    </v-toolbar-items>
+
+    <v-spacer></v-spacer>
+
+    <v-toolbar-items>
+      <v-btn flat>Finalizar dias</v-btn>
+      <v-menu offset-y>
+        <v-btn flat slot="activator">salvar e carregar</v-btn>
+        <v-list>
+          <v-list-tile>
+            <v-list-title>Salvar Dados</v-list-title>
+          </v-list-tile>
+          <v-list-tile>
+            <v-list-title>Carregar Dados</v-list-title>
+          </v-list-tile>
+        </v-list>
+      </v-menu>
+    </v-toolbar-items>
+
+  </v-toolbar>
 </template>
 
 <script>
