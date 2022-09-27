1c8760f4b993a56c87d3c5e69360326f7361b8b6
Horecio Araujo Dias <horecio@gmail.com>
2022-09-26 22:45:47 -0300

    create home componente with fund and add fund in header too

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,34 @@
 <template>
-  <h1>Início</h1>
+  <div>
+    <h1 class="display-3 font-weight-light mb-4">
+      Negocie e Consulte suas Ações
+    </h1>
+    <v-sheet :elevator="6" class="pa-2" color="primary">
+      <v-icon class="white--text mr-3">info</v-icon>
+      <span class="headline white--text font-weight-light">
+        Você pode Salvar & Carregar os Dados
+      </span>
+    </v-sheet>
+    <v-sheet :elevator="6" class="pa-2 mt-3" color="success darken-1">
+      <v-icon class="white--text mr-3">info</v-icon>
+      <span class="headline white--text font-weight-light">
+        Clique em 'Finalizar Dia' para iniciar um novo dia!
+      </span>
+    </v-sheet>
+    <v-divider class="my-4"></v-divider>
+    <p class="display-1 font-weight-light"><strong>Seu Saldo:</strong>{{ funds }}</p>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  computed: {
+    funds() {
+      return this.$store.getters.funds
+    }
+  },
+};
 </script>
 
 <style>
-
 </style>
